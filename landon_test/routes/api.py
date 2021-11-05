f36e97bc5b63af5ba45b9d9444aa4dc66cb020eb
Landon Newberry <landonnewberry@gmail.com>
2021-11-04 20:15:14 -0700

    update

--- landon_test/routes/api.py
+++ landon_test/routes/api.py
@@ -16,4 +16,4 @@ def get_status():
     description="Get root."
 )
 def get_status():
-    return PlainTextResponse(status_code=200, content="LANDON TEST MERGE DEPLOY")
+    return PlainTextResponse(status_code=200, content="IT WORKED")
