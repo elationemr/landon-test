68235c40692cf0daf2c84b4b211bd47d7b4136aa
Landon Newberry <landonnewberry@gmail.com>
2021-11-04 20:27:59 -0700

    IT EXTRA WORKED

--- landon_test/routes/api.py
+++ landon_test/routes/api.py
@@ -16,4 +16,4 @@ def get_status():
     description="Get root."
 )
 def get_status():
-    return PlainTextResponse(status_code=200, content="IT WORKED")
+    return PlainTextResponse(status_code=200, content="IT EXTRA WORKED")
